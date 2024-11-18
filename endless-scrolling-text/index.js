d73651c30c94d7222cbf7db64580d1d059c1bb3a
1bit2bits4bits8bit <Bernardodanielmo12raes123@gmail.com>
2024-11-18 10:04:01 -0300

    what?

--- endless-scrolling-text/index.js
+++ endless-scrolling-text/index.js
@@ -1,2 +1,2 @@
 const scrollingText = document.getElementById('scrollingText');
-scrollingText.innerHTML = "This is a useless site. ".repeat(100);  // Repeat the text to fill the screen
+scrollingText.innerHTML = "This is a useless site. ".repeat(9999);  // Repeat the text to fill the screen
